2b09b53b64c9f1e255b099fcf1eec4a4ba05800f
michael dye <m-github@divisive.info>
2017-10-10 14:24:36 -0600

    made node installation selective per platform

--- build_support/tools/Dockerfile-render-bld.bash
+++ build_support/tools/Dockerfile-render-bld.bash
@@ -13,10 +13,13 @@ DEST="$3"
 
 golang_dl_url=""
 from_image=""
+# the default Docker instruction for architectures supported by it
+node_install="RUN curl -sL https://deb.nodesource.com/setup_4.x | bash - \&\& apt-get install -y nodejs"
 
 case "$ARCH" in
 ('armhf')
   from_image="arm32v7/ubuntu:16.04"
+  node_install="RUN apt-get install -y nodejs npm"
   golang_dl_url="https://storage.googleapis.com/golang/go1.9.1.linux-armv6l.tar.gz"
   ;;
 ('arm64')
@@ -29,6 +32,7 @@ case "$ARCH" in
   ;;
 ('ppc64el')
   from_image="ppc64le/ubuntu:16.04"
+  node_install="RUN apt-get install -y nodejs npm"
   golang_dl_url="https://storage.googleapis.com/golang/go1.9.1.linux-ppc64le.tar.gz"
   ;;
 (*)
@@ -37,6 +41,7 @@ case "$ARCH" in
   ;;
 esac
 
-sed "s|##from_image##|$from_image|" "$FNAME" > "$DEST"
-sed -i.bak "s|##arch##|$ARCH|" "$DEST" && rm -f "$DEST".bak
-sed -i.bak "s|##golang_dl_url##|$golang_dl_url|" "$DEST" && rm -f "$DEST".bak
+sed "s,##from_image##,$from_image," "$FNAME" > "$DEST"
+sed -i.bak "s,##arch##,$ARCH," "$DEST" && rm -f "$DEST".bak
+sed -i.bak "s,##node_install##,$node_install," "$DEST" && rm -f "$DEST".bak
+sed -i.bak "s,##golang_dl_url##,$golang_dl_url," "$DEST" && rm -f "$DEST".bak
