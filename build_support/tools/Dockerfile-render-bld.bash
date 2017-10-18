966415187ca276b960417f94a54e5b8fd45b86d3
michael dye <m-github@divisive.info>
2017-10-18 17:22:14 -0600

    fixed bug in declared golang architectures for download

--- build_support/tools/Dockerfile-render-bld.bash
+++ build_support/tools/Dockerfile-render-bld.bash
@@ -20,26 +20,28 @@ case "$ARCH" in
 ('armhf')
   from_image="arm32v7/ubuntu:16.04"
   node_install="RUN apt-get install -y nodejs npm"
-  golang_dl_url="https://storage.googleapis.com/golang/go1.9.1.linux-armv6l.tar.gz"
+  golang_arch="armv6l"
   ;;
 ('arm64')
   from_image="arm64v8/ubuntu:16.04"
-  golang_dl_url="https://storage.googleapis.com/golang/go1.9.linux-arm64.tar.gz"
+  golang_arch="arm64"
   ;;
 ('amd64')
   from_image="ubuntu:16.04"
-  golang_dl_url="https://storage.googleapis.com/golang/go1.9.1.linux-amd64.tar.gz"
+  golang_arch="amd64"
   ;;
 ('ppc64el')
   from_image="ppc64le/ubuntu:16.04"
   node_install="RUN apt-get install -y nodejs npm"
-  golang_dl_url="https://storage.googleapis.com/golang/go1.9.1.linux-ppc64le.tar.gz"
+  golang_arch="ppc64le"
   ;;
 (*)
   (>&2 echo "Unknown or unsupported architecture: $1")
   exit 1
   ;;
 esac
+
+golang_dl_url="https://storage.googleapis.com/golang/go1.9.1.linux-${golang_arch}.tar.gz"
 
 sed "s,##from_image##,$from_image," "$FNAME" > "$DEST"
 sed -i.bak "s,##arch##,$ARCH," "$DEST" && rm -f "$DEST".bak
