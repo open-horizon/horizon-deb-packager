7ac7bed61b847c6a327ec6ad58c8fa66f98a73fc
linggao <linggao@us.ibm.com>
2017-12-15 21:57:40 +0000

    added a newly modified wiotp file

--- pkgsrc/seed/horizon-wiotp/fs/usr/wiotp-edge/bin/wiotp_agent_setup.sh
+++ pkgsrc/seed/horizon-wiotp/fs/usr/wiotp-edge/bin/wiotp_agent_setup.sh
@@ -29,10 +29,17 @@ Arguments:
     (Required) Device or Gateway Token.
 
   -r <val>, --region <val>, --region=<val>
-    (Optional) Organization Region: us | uk. Default is us.
+    (Optional) Organization Region: us | uk | ch | de . Default is us.
+    Use us for US South and East
+    Use uk for United Kingdom
+    Use ch for China
+    Use de for Germany
 
   -dm <val>, --domain <val>, -domain=<val>
     (Optional) WIoTP internet domain. Default is internetofthings.ibmcloud.com.
+
+  -cn <val>, --edgeCN <val>, -edgeCN=<val>
+    (Optional) Common Name (CN) to be used when generating the Server Certificate for the Edge Connector.
 
   --cloudDisableCertCheck <true|false>, --cloudDisableCertCheck=<true|false>
     (Optional) Sets the CloudDisableCertCheck property for the Edge Connector configuration file. Using true will ignore non-trusted server certificates. 
@@ -74,6 +81,7 @@ while [ "$#" -gt 0 ]; do
         -r|--region) shift; WIOTP_INSTALL_REGION=$1;;
         -dm|--domain) shift; WIOTP_INSTALL_DOMAIN=$1;;
         -cdc|--cloudDisableCertCheck) shift; CDCC_TEMP=$1;;
+        -cn|--edgeCN) shift; WIOTP_INSTALL_EDGE_CN=$1;;
         -h|--help) usage; exit 0;;
         -*) usage_fatal "unknown option: '$1'";;
         *) break;; # reached the list of file names
@@ -86,23 +94,28 @@ done
 if [[ -z $WIOTP_INSTALL_DEVICE_ID ]] || [[ -z $WIOTP_INSTALL_DEVICE_TYPE ]] || [[ -z $WIOTP_INSTALL_DEVICE_ID ]] || [[ -z $WIOTP_INSTALL_DEVICE_TOKEN ]]; then
   usage_fatal "Values for the following options are required: --org, --deviceType, --deviceId, --deviceToken"
 fi
+# Check valid regions
+if [[ "${WIOTP_INSTALL_REGION}" != "us" && "${WIOTP_INSTALL_REGION}" != "uk" && "${WIOTP_INSTALL_REGION}" != "de" && "${WIOTP_INSTALL_REGION}" != "ch" ]];then
+    usage_fatal "Invalid region."
+fi
 
 WIOTP_INSTALL_EC_DISABLE_CERT_CHECK=false
 case $CDCC_TEMP in
   (true)    WIOTP_INSTALL_EC_DISABLE_CERT_CHECK=true;;
 esac
 
-fancyLog "$0 script arguments:"
-echo WIOTP_INSTALL_ORGID=$WIOTP_INSTALL_ORGID
-echo WIOTP_INSTALL_DEVICE_TYPE=$WIOTP_INSTALL_DEVICE_TYPE
-echo WIOTP_INSTALL_DEVICE_ID=$WIOTP_INSTALL_DEVICE_ID
-echo WIOTP_INSTALL_DEVICE_TOKEN=$WIOTP_INSTALL_DEVICE_TOKEN
-if [ ! -z WIOTP_INSTALL_TEST_ENV ]; then
-  echo WIOTP_INSTALL_TEST_ENV=$WIOTP_INSTALL_TEST_ENV
-fi
-echo WIOTP_INSTALL_REGION=$WIOTP_INSTALL_REGION
-echo WIOTP_INSTALL_DOMAIN=$WIOTP_INSTALL_DOMAIN
-echo WIOTP_INSTALL_EC_DISABLE_CERT_CHECK=$WIOTP_INSTALL_EC_DISABLE_CERT_CHECK
+# fancyLog "$0 script arguments:"
+# echo WIOTP_INSTALL_ORGID=$WIOTP_INSTALL_ORGID
+# echo WIOTP_INSTALL_DEVICE_TYPE=$WIOTP_INSTALL_DEVICE_TYPE
+# echo WIOTP_INSTALL_DEVICE_ID=$WIOTP_INSTALL_DEVICE_ID
+# echo WIOTP_INSTALL_DEVICE_TOKEN=$WIOTP_INSTALL_DEVICE_TOKEN
+# if [ ! -z WIOTP_INSTALL_TEST_ENV ]; then
+#   echo WIOTP_INSTALL_TEST_ENV=$WIOTP_INSTALL_TEST_ENV
+# fi
+# echo WIOTP_INSTALL_REGION=$WIOTP_INSTALL_REGION
+# echo WIOTP_INSTALL_DOMAIN=$WIOTP_INSTALL_DOMAIN
+# echo WIOTP_INSTALL_EC_DISABLE_CERT_CHECK=$WIOTP_INSTALL_EC_DISABLE_CERT_CHECK
+# echo WIOTP_INSTALL_EDGE_CN=$WIOTP_INSTALL_EDGE_CN
 
 function checkrc {
 	if [[ $1 -ne 0 ]]; then
@@ -113,21 +126,52 @@ function checkrc {
 
 fancyLog "WIoTP Horizon agent setup"
 
-INSTALL_PATH=$PWD
-echo "Install path: $INSTALL_PATH"
 VAR_DIR="/var"
 ETC_DIR="/etc"
 
-
 if [ -z $WIOTP_INSTALL_TEST_ENV ]; then
   httpDomainPrefix=$WIOTP_INSTALL_ORGID
+  httpDomain=$WIOTP_INSTALL_DOMAIN
   mqttDomainPrefix=$WIOTP_INSTALL_ORGID.messaging
   regionPrefix=$WIOTP_INSTALL_REGION
 else
   httpDomainPrefix=$WIOTP_INSTALL_ORGID.$WIOTP_INSTALL_TEST_ENV
+  httpDomain=$WIOTP_INSTALL_TEST_ENV.$WIOTP_INSTALL_DOMAIN
   mqttDomainPrefix=$WIOTP_INSTALL_ORGID.messaging.$WIOTP_INSTALL_TEST_ENV
   regionPrefix=$WIOTP_INSTALL_REGION.$WIOTP_INSTALL_TEST_ENV
 fi
+
+# Check if domain exists
+log "Checking domain..."
+output=$(curl -Is -o /dev/null -w "%{http_code}" https://$httpDomain)
+if [[ $output -ne 200 ]]; then
+  fatal "Invalid domain $httpDomain. Could not reach https://$httpDomain"
+fi
+
+log "Domain is valid."
+
+# Check if org exists
+log "Checking org..."
+output=$(curl -Is -o /dev/null -w "%{http_code}" https://$httpDomainPrefix.$WIOTP_INSTALL_DOMAIN)
+if [[ $output -ne 200 ]]; then
+  fatal "Invalid org $WIOTP_INSTALL_ORGID. Could not reach https://$httpDomainPrefix.$WIOTP_INSTALL_DOMAIN. Check the value passed to --org"
+fi
+
+log "Org is valid."
+
+log "Checking device id and device type..."
+output=$(curl -s -o /dev/null -w "%{http_code}" -u "$WIOTP_INSTALL_ORGID/g@$WIOTP_INSTALL_DEVICE_TYPE@$WIOTP_INSTALL_DEVICE_ID:$WIOTP_INSTALL_DEVICE_TOKEN" https://$httpDomainPrefix.$WIOTP_INSTALL_DOMAIN/api/v0002/edgenode/orgs/$WIOTP_INSTALL_ORGID/nodes/g@$WIOTP_INSTALL_DEVICE_TYPE@$WIOTP_INSTALL_DEVICE_ID)
+if [[ $output -ne 200 ]]; then
+  log "ERROR: Could not access device $WIOTP_INSTALL_DEVICE_ID of type $WIOTP_INSTALL_DEVICE_TYPE."
+  log "  Troubleshooting:"
+  log "    1. Check if the device type was created in Watson IoT Platform."
+  log "    2. Make sure that when device type was created, 'Gateway' type was selected and 'Edge Cababilities' toggle was enabled."
+  log "    3. Check if a device was created under that device type."
+  log "    4. Make sure the device credentials are correct."
+  exit 1
+fi
+
+log "Device id, device type and device authentication token are valid."
 
 # Read the json object in /etc/horizon/anax.json
 anaxJson=$(jq '.' $ETC_DIR/horizon/anax.json)
@@ -185,7 +229,11 @@ echo "$configJson" > $ETC_DIR/wiotp-edge/hznEdgeCoreIoTInput.json
 # Generate edge-mqttbroker certificates
 mkdir -p ${VAR_DIR}/wiotp-edge/persist/
 fancyLog "Generating Edge internal certificates" 
-wiotp_create_certificate "/etc/wiotp-edge/edge.conf" $WIOTP_INSTALL_DEVICE_TOKEN
+if [[ -z $WIOTP_INSTALL_EDGE_CN ]]; then
+  wiotp_create_certificate -p $WIOTP_INSTALL_DEVICE_TOKEN
+else  
+  wiotp_create_certificate -p $WIOTP_INSTALL_DEVICE_TOKEN -cn $WIOTP_INSTALL_EDGE_CN
+fi
 
 fancyLog "Waiting for Horizon service to restart ..."
 sleep 1
